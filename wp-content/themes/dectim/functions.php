fdf27a85c406afe056dc00cbdbddfd3731ce378f
mstonge <maxonge95@gmail.com>
2015-12-11 10:35:28 -0500

    Enseignant
    CustomPost

--- wp-content/themes/dectim/functions.php
+++ wp-content/themes/dectim/functions.php
@@ -524,7 +524,7 @@ function etudiants() {
 		'show_ui'               => true,
 		'show_in_menu'          => true,
 		'menu_position'         => 5,
-		'menu_icon'             => ' dashicons-universal-access-alt',
+		'menu_icon'             => 'dashicons-universal-access-alt',
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
 		'can_export'            => true,
@@ -537,3 +537,50 @@ function etudiants() {
 
 }
 add_action( 'init', 'etudiants', 0 );
+// Register Custom Post Type
+function enseignants() {
+
+	$labels = array(
+		'name'                  => _x( 'Enseignants', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Enseignant', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Enseignants', 'text_domain' ),
+		'name_admin_bar'        => __( 'Enseignants', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Items', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Item', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Item', 'text_domain' ),
+		'edit_item'             => __( 'Edit Item', 'text_domain' ),
+		'update_item'           => __( 'Update Item', 'text_domain' ),
+		'view_item'             => __( 'View Item', 'text_domain' ),
+		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Enseignant', 'text_domain' ),
+		'description'           => __( 'Liste des enseignants de Dectim', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'custom-fields', 'page-attributes', 'post-formats', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-universal-access',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'enseignants', $args );
+
+}
+add_action( 'init', 'enseignants', 0 );
