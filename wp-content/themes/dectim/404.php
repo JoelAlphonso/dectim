239968f138973575f1f47c5dba83aca4e1071fa2
Olon11 <olivier_brochu@hotmail.com>
2015-12-27 18:41:36 -0500

    404 et pied de page
    Conversion de la page 404 et du pied de page vers Worpdress

--- wp-content/themes/dectim/404.php
+++ wp-content/themes/dectim/404.php
@@ -1,5 +1,19 @@
 <?php get_header(); ?>
-	<main>
-		<h1>404</h1>
+	<main class="p404">
+		<h1><span>Erreur</span></h1>		
+		<h1>Page 404</h1>
+		<?php
+			#Si on a un lien vers une autre page
+			if (get_field("lien_vers_une_autre_page")):
+				#Trouver le target
+				$target = empty(get_field("lien_externe")) ? "":'target="_blank"';
+				
+				#Trouver l'url
+				$lien = empty(get_field("lien_externe")) ? get_field("lien_vers_une_page"):get_field("url_du_lien");
+		?>
+				<p><a href="<?php echo $lien; ?>" <?php echo $target; ?>><?php the_field("texte_du_lien"); ?></a></p>
+		<?php
+			endif;
+		?>
 	</main>
 <?php get_footer(); ?>
--- wp-content/themes/dectim/footer.php
+++ wp-content/themes/dectim/footer.php
@@ -1,23 +1,29 @@
-	
 	<footer>
 		<div class='footerBack'></div>
 		<div class='footerFront'></div>
-			<div class='content'>
-				<div class='sociaux'>
-					<ul>
-						<li id='Ficon'><a href=''><h3>FACEBOOK</h3></a></li>
-						<li id='Yicon'><a href=''><h3>YOUTUBE</h3></a></li>
-						<li id='Licon'><a href=''><h3>LINKEDIN</h3></a></li>
-						<li id='Dicon'><a href=''><h3>DEVIANTART</h3></a></li>
-					</ul>
-				</div>
-				<div class='logos'>
-				<a href='http://www.cegepmontpetit.ca/'>
-					<img src="<?php bloginfo("template_url")?>/img/dectimlogo.png" alt='TimEdouard'>
-					</a>
-				</div>
+		<div class='content'>
+			<div class='sociaux'>
+				<ul>
+					<?php
+						#Aller chercher les réseaux sociaux
+						if (have_rows("reseaux_sociaux")):
+							while (have_rows("reseaux_sociaux")):
+								the_row();
+					?>
+								<li class="<?php echo strtolower(get_sub_field("reseau_social")); ?>"><a href="<?php the_sub_field("url_socialmedia"); ?>" target="_blank"><h3><?php the_sub_field("reseau_social"); ?></h3></a></li>
+					<?php			
+							endwhile;
+						endif;
+					?>
+				</ul>
 			</div>
-		</footer>
+			<div class='logos'>
+				<a href='<?php the_field("url_du_cegep"); ?>' target="_blank">
+					<img src="<?php bloginfo("template_url")?>/img/dectimlogo.png" alt='TimEdouard'>
+				</a>
+			</div>
+		</div>
+	</footer>
 		<!--CDN links for TweenLite, CSSPlugin, and EasePack-->
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/plugins/CSSPlugin.min.js"></script>
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/easing/EasePack.min.js"></script>
